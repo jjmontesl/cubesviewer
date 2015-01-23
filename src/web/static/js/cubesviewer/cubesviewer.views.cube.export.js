1e8ae3887d0eae5f957f4479903663e79698cb95
José Juan Montes <jjmontes@zentyal.com>
2015-01-23 16:21:17 +0100

    Fixed export facts button (broken since change to cubes.js lib)

--- src/web/static/js/cubesviewer/cubesviewer.views.cube.export.js
+++ src/web/static/js/cubesviewer/cubesviewer.views.cube.export.js
@@ -11,11 +11,11 @@
  *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * If your version of the Software supports interaction with it remotely through
  * a computer network, the above copyright notice and this permission notice
  * shall be accessible to all users.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,13 +26,13 @@
  */
 
 /*
- * This addon adds export to CSV capability to CubesViewer cube view. 
+ * This addon adds export to CSV capability to CubesViewer cube view.
  * It offers an "export facts" menu option for all cube view modes,
- * and a "export table" option in Explore and Series mode. 
+ * and a "export table" option in Explore and Series mode.
  */
 function cubesviewerViewCubeExporter() {
 
-	this.cubesviewer = cubesviewer; 
+	this.cubesviewer = cubesviewer;
 
 	/*
 	 * Draw export options.
@@ -40,32 +40,32 @@ function cubesviewerViewCubeExporter() {
 	this.onViewDraw = function(event, view) {
 
 		//if (view.params.mode != "explore") return;
-		
+
 		view.cubesviewer.views.cube.exporter.drawMenu(view);
-		
+
 	};
 
 	/*
 	 * Draw export menu options.
 	 */
 	this.drawMenu = function(view) {
-		
+
 		if (view.cube == null) return;
-		
+
 		var menu = $(".cv-view-menu-view", $(view.container));
 		var cube = view.cube;
-		
+
 		// Draw menu options (depending on mode)
-		
+
 		menu.append('<div></div>');
 		if ((view.params.mode == "explore") || (view.params.mode == "series")) {
 			menu.append('<li><a href="#" class="cv-view-export-table"><span class="ui-icon ui-icon-script"></span>Export table</a></li>');
 		}
 		menu.append('<li><a href="#" class="cv-view-export-facts"><span class="ui-icon ui-icon-script"></span>Export facts</a></li>');
-	  		
+
 		$(menu).menu( "refresh" );
 		$(menu).addClass("ui-menu-icons");
-		
+
 		// Events
 		$(view.container).find('.cv-view-export-table').click(function() {
 			view.cubesviewer.views.cube.exporter.exportCsv(view);
@@ -74,17 +74,22 @@ function cubesviewerViewCubeExporter() {
 		$(view.container).find('.cv-view-export-facts').click(function() {
 			view.cubesviewer.views.cube.exporter.exportFacts(view);
 			return false;
-		});		
-		
-		
-	};	
-	
+		});
+
+
+	};
+
 	/*
 	 * Download facts in CSV format from Cubes Server
 	 */
 	this.exportFacts = function(view) {
 
-		var params = view.cubesviewer.views.cube.buildQueryParams(view, false, true);
+		var params = {}
+		var args = view.cubesviewer.views.cube.buildBrowserArgs(view, false, true);
+
+		params = args;
+        if(args && args.cut)
+            params.data.cut = params.data.cut.toString();
 
 		params["format"] = "csv";
 
@@ -98,24 +103,24 @@ function cubesviewerViewCubeExporter() {
 	 * Export a view (either in "explore" or "series" mode) in CSV format.
 	 */
 	this.exportCsv = function (view) {
-		
+
 		var content = "";
-		
+
 		if (view.params.mode == "explore") {
 			var grid = $('#summaryTable-' + view.id);
 		} else {
 			var grid = $('#seriesTable-' + view.id);
 		}
-		
+
 		var values = [];
 		for (var i = ((view.params.mode == "explore") ? 1 : 0); i < grid.jqGrid('getGridParam','colNames').length; i++) {
 			values.push ('"' + grid.jqGrid('getGridParam','colNames')[i] + '"');
 		}
 		content = content + (values.join(",")) + "\n";
-		
+
 		//var m = grid.getDataIDs();
 		var m = grid.jqGrid('getGridParam', 'data');
-		
+
 		for (var i = 0; i < m.length; i++) {
 		    var record = m[i];
 		    values = [];
@@ -130,7 +135,7 @@ function cubesviewerViewCubeExporter() {
 			}
 		    content = content + (values.join(",")) + "\n";
 		}
-		
+
 		var url = "data:text/csv;charset=utf-8," + encodeURIComponent(content);
 		window.open (url, "_blank");
 	};
