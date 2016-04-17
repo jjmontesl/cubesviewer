011ac801546b731fb152fb534f74687e21eab222
Jose Juan Montes <jjmontes@gmail.com>
2016-04-17 10:33:38 +0200

    Updating Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function(grunt) {
         separator: ';'
       },
       dist: {
-        src: ['../web/static/js/cubesviewer/**/*.js'],
-        dest: '../target/cubesviewer/<%= pkg.name %>.js'
+        src: ['src/web/static/js/cubes/**/*.js', 'src/web/static/js/cubesviewer/**/*.js'],
+        dest: 'build/<%= pkg.name %>.js'
       }
     },
     uglify: {
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
     },
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
       files: ['test/**/*.html']
     },
     jshint: {
-      files: ['Gruntfile.js', '../web/static/js/cubesviewer/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'src/web/static/js/cubesviewer/**/*.js', 'test/**/*.js'],
       options: {
         // options here to override JSHint defaults
         globals: {
@@ -44,13 +44,13 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  //grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
 
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify']); // 'jshint'
 
 };
 
